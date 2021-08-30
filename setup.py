5b4a82076acf8c0edb09c61f79b32bc97078bb36
AtaiDobrynin <atay.dobrynin@gmail.com>
2021-08-30 13:37:46 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scmoib",
+    version="0.0.1",
+    author="Atai Dobrynin",
+    author_email="atay.dobrynin@gmail.com",
+    description="SCMOIB package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/colomemaria/scmoib-package",
+    project_urls={
+        "Bug Tracker": "https://github.com/colomemaria/scmoib-package/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "scmoib"},
+    packages=setuptools.find_packages(where="scmoib"),
+    python_requires=">=3.6",
+)
