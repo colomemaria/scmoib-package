fd3f43d9e6657a14f1bcf94c3856104624b4e7ec
Anna Danese <anna.danese@helmholtz-muenchen.de>
2021-09-02 17:10:22 +0200

    comments

--- scmoib/plotting/_node_distr.py
+++ scmoib/plotting/_node_distr.py
@@ -4,6 +4,11 @@ import seaborn as sns
 
 
 def node_distr(adata, title=None):
+	"""
+	Historgram plotting the number of nodes between matching barcodes. 
+	
+	"""
+
     v1, v2 = np.unique(adata.uns['nodes_count'], return_counts=True)
     fig, ax = plt.subplots(figsize=(10, 5))
     sns.barplot(x=v1, y=v2, ax=ax)
--- scmoib/plotting/_umap.py
+++ scmoib/plotting/_umap.py
@@ -1,5 +1,11 @@
 import scanpy as sc
 
 
-def umap(adata, color=None):
+def umap_barcodes(adata, color=None):
+	"""
+	instead of plotting the edge of the knn graph what we want is to draw an 
+	edge between the matching barcodes.
+
+	@ Anna : DO IT DAMNED
+	"""
     sc.pl.umap(adata, color=color, edges=True)
