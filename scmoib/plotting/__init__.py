c5a509991631b1f53659963cd7c48638308288d7
Anna Danese <anna.danese@helmholtz-muenchen.de>
2022-02-28 12:28:59 +0100

    update umap_barcode

--- scmoib/plotting/__init__.py
+++ scmoib/plotting/__init__.py
@@ -8,7 +8,10 @@ from ._metric_heatmap import metric_heatmap
 from ._node_distr import node_distr
 from ._river_plot import river_plot
 from ._river_plot_2_omics import river_plot_2_omics
-from ._umap_barcodes import umap_barcodes
+# add a if scanpy<1.8 :
+#from ._umap_barcodes import umap_barcodes
+# else: 
+from ._umap_barcodes_2 import umap_barcodes
 from ._pairwise_distance import pairwise_distance
 from ._silhouette import silhouette
 from ._metrics_scatterplot import metrics_scatterplot
---
+++ scmoib/plotting/_umap_barcodes_2.py
@@ -0,0 +1,34 @@
+import scanpy as sc
+
+def umap_barcodes(
+        adata: AnnData,
+        bc_list1: Iterable[str],
+        bc_list2: Iterable[str],
+        basis: str = 'umap',
+        color: Union[str, Sequence[str], None] = None,
+        edges: bool = True,
+        edges_width: float = 0.1
+) -> None:
+    """
+
+    Parameters
+    ----------
+    adata
+        Annotated data matrix
+    bc_list1
+        List of barcodes
+    bc_list2
+        List of barcodes matched with bc_list1
+    basis
+        Name of the `obsm` basis to use.
+    color
+
+    edges
+        If True shows the edges between matched barcodes.
+    edges_width
+        Width of edges.
+    """
+    bc_list = list(adata.obs.index)
+    edge_list = list(zip([bc_list.index(i) for i in bc_list1], [bc_list.index(j) for j in bc_list2]))
+    sc.plotting._tools.scatterplots.embedding(adata, basis=basis, color=color, edges=edges, edges_width=edges_width, edge_list=edge_list)
+
